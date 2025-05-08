be932919c83de240caa4544d93c785bd884d53f5
Jay Rosato <rosato.jay@gmail.com>
2025-05-08 14:52:40 +0200

    Added filter and search abilities.
    This commit adds filter and search abilities to tables, and slightly alters the sort to make
    it slightly more robust.

--- backend/bdvBackend/static/js/sort-tables.js
+++ backend/bdvBackend/static/js/sort-tables.js
@@ -1,4 +1,71 @@
-window.onload = () => addClickable()
+document.addEventListener('DOMContentLoaded', async function () {
+    addClickable()
+    const search = document.querySelector('.search-records')
+    search.addEventListener('change', () => searchRecords())
+    createFilters()
+});
+
+function createFilters(){
+    let table = document.querySelector('.sortable-table');
+    const headers = table.querySelectorAll('th')
+    headers.forEach((h, index) =>{
+        const filter = h.querySelector('select')
+        if(filter){
+            const tableBody = table.tBodies[0]
+            const values = []
+            const rowsArray = Array.from(tableBody.rows)
+            rowsArray.forEach((row) => {
+                let value = row.cells[index].textContent
+                if(!values.includes(value))
+                values.push(value)
+            })
+            const defaultOption = document.createElement('option')
+            defaultOption.innerText = '-----'
+            defaultOption.value = ''
+            filter.appendChild(defaultOption)
+            values.forEach((value) => {
+                const option = document.createElement('option')
+                option.innerText = value
+                option.value = value
+                filter.appendChild(option)
+            })
+            filter.onchange = () => applyFilter(index, filter)
+        }
+    })
+}
+
+function applyFilter(col, filter){
+    const filterValue = filter.value
+    let table = document.querySelector('.sortable-table');
+    const tableBody = table.tBodies[0]
+    const rowsArray = Array.from(tableBody.rows)
+    rowsArray.forEach((row) => {
+        let value = row.cells[col].textContent
+        if(value == filterValue || filterValue == ''){
+            row.style.display = ''
+        }
+        else{
+            row.style.display = 'none'
+        }
+    });
+}
+function searchRecords(){
+    const search = document.querySelector('.search-records')
+    const searchValue = search.value.trim().toLowerCase()
+    let table = document.querySelector('.sortable-table');
+    const tableBody = table.tBodies[0]
+    const rowsArray = Array.from(tableBody.rows)
+    rowsArray.forEach((row) => {
+        let value = row.cells[0].textContent.trim().toLowerCase()
+        if(value.includes(searchValue) || searchValue == ''){
+            row.style.display = ''
+        }
+        else{
+            row.style.display = 'none'
+        }
+    });
+    
+}
 
 function addClickable(){
     let table = document.querySelector('.sortable-table');
@@ -9,30 +76,52 @@ function addClickable(){
     const headers = table.querySelectorAll('th')
     headers.forEach((h, index) => {
         headerTracker.push({h:true})
-        h.onclick = () => {
-            let sortDir = headerTracker[index]
-            headerTracker[index] = !sortDir
-            sortTable(index, sortDir)
+        const sorter = h.querySelector('button')
+        if(sorter){
+            sorter.onclick = () => {
+                let sortDir = headerTracker[index]
+                headerTracker[index] = !sortDir
+                sortTable(index, sortDir)
+            }
         }
     })
 }
 
-function sortTable(index, asc=true){
+function sortTable(i, asc=true){
     let table = document.querySelector('.sortable-table');
     const tableBody = table.tBodies[0]
     const rowsArray = Array.from(tableBody.rows)
-
+    const headers = table.querySelectorAll('th')
+    let sortType = headers[i].getAttribute('sort')
+    if(!sortType){sortType='text'}
+    
     rowsArray.sort((current, next) => {
-        const currentVal = current.cells[index].textContent.trim().toLowerCase()
-        const nextVal = next.cells[index].textContent.trim().toLowerCase()
+        let currentVal = current.cells[i].textContent.trim()
+        let nextVal = next.cells[i].textContent.trim()
+        if(sortType == 'text'){
+            currentVal = currentVal.toLowerCase()
+            nextVal = nextVal.toLowerCase()
+        }
+        if(sortType == 'number'){
+            currentVal = parseFloat(currentVal)
+            nextVal = parseFloat(nextVal)
+        }
+        if(sortType == 'date'){
+            currentVal = new Date(currentVal)
+            nextVal = new Date(nextVal)
+            if (isNaN(currentVal) || isNaN(nextVal)) return 0;
+        }
         if(currentVal < nextVal) return asc ? -1:1;
         if(currentVal > nextVal) return asc ? 1:-1;
         return 0
     });
     rowsArray.forEach(row => tableBody.appendChild(row));
 
-    const headers = table.querySelectorAll('th');
-    let header = headers[index];
-    header.textContent = header.textContent.replace('↑', '').replace('↓', '');
-    header.textContent += asc ? '↑' : '↓';
+    headers.forEach((h, index)=>{
+        const sorter = h.querySelector('button')
+        if(sorter){
+            sorter.innerText = sorter.innerText.replace('↑', 'S').replace('↓', 'S')
+            index == i ? sorter.innerText = asc ? '↑' : '↓': null;
+        }
+    })
 }
