85376543bccc83a78f7bc3e30b88c390f0938af6
Jay Rosato <rosato.jay@gmail.com>
2025-06-05 13:50:29 +0200

    Added whitenoise to load static.

--- bdvBackend/settings.py
+++ bdvBackend/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
 ]
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -147,6 +148,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
