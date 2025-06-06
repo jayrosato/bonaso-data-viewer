211984b26c2a6fe35e43abb6b406c67b50c3cd83
Jay Rosato <rosato.jay@gmail.com>
2025-06-06 08:35:25 +0200

    Trying to get changes to reflect...

--- bdvBackend/settings.py
+++ bdvBackend/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
